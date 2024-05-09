0221fcc3069fba1bb19aa48814a8e61cb48ac660
carlivan <carlhiselius@gmail.com>
2024-05-09 20:08:04 +0200

    added script

---
+++ setvcvars.ps1
@@ -0,0 +1,9 @@
+pushd "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build"
+cmd /c "vcvars64.bat&set" |
+foreach {
+  if ($_ -match "=") {
+    $v = $_.split("=", 2); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])" 
+  }
+}
+popd
+Write-Host "`nVisual Studio 2022 Command Prompt variables set." -ForegroundColor Yellow
